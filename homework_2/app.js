c4c9ce775b35d8884c60e4b4ae3a0ef1598ef8c2
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2022-07-20 21:30:41 +0300

    router/post,put,delete

--- homework_2/app.js
+++ homework_2/app.js
@@ -8,9 +8,9 @@ const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json()); // для контент тайп джейсон в req.body (інакше відповідь в req.body - андефайнд)
 
 app.use('/api/contacts', contactsRouter)
 
@@ -18,7 +18,7 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
 
-app.use((err, req, res, next) => {
+app.use((err, req, res, next) => {   // мідлвара для обробки помилок (тому що приймає 4 параметри)
   const { status = 500, message = "Server error"} = err;
   res.status(status).json({ message });
 })
